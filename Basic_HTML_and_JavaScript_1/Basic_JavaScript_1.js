c24e80946544a114e749982d78db8bcb58351df5
MBailor <65557271+MBailor@users.noreply.github.com>
2020-05-19 13:05:18 -0700

    Pusing my Basic_HTML_and_JavaScript folder to my instructors at The Tech Academy

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Today will be awesome! Youwill conquor the world. Click OK if you agree.");
+
+
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
