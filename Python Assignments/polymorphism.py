6568b4bf9365c3f0145a196d668378d0097f917e
MBailor <65557271+MBailor@users.noreply.github.com>
2020-08-11 11:54:43 -0700

    Update polymorphism.py
    Trying agin with Polymorphism

--- Python Assignments/polymorphism.py
+++ Python Assignments/polymorphism.py
@@ -3,8 +3,8 @@
 
 #Parent Class is Banker
 class Banker:
-    name = "Jimmy"
-    department = "Teller"
+    name = "Billy"
+    department = "Manager"
     empNum = "3809"
 
 #           This is the method of the parent class
@@ -17,43 +17,46 @@ class Banker:
         else:
             print("Incorrect Employee Number. Security has been notified!!\n")
 
-#Child Class is Member
-class Member:
-    name = "Dirk"
-    birthPlace = "Seattle"
-    pinNum = "2445"
+class Underling(Banker):
+    name = "Jimmy"
+    department = "Teller"
+    email = "JWe@mail.com"
+    empPin = "2445"
 
-#           Same method that the parent uses except parent asked for employee number and child askes for a pin number
     def loginInfo(self):
-        entry_name = input("Enter Member Name: \n")
-        entry_birthPlace = input("City of Birth: \n")
+        entry_name = input("Employee Name: \n")
+        entry_department = input("Job Title: \n")
+        entry_empPin = input("Employee Number: \n")
+        if (entry_department == self.department and entry_empPin == self.empPin):
+            print("Welcome back, {} Now GET BACK TO WORK!\n".format(entry_name))
+        else:
+            print("Incorrect Employee Number. Security has been notified!!\n")
+    
+
+class Member(Banker):
+    userName = "Marcy"
+    department = ""
+    email = "JWe@mail.com"
+    pinNum = "2890"
+
+    def loginInfo(self):
+        entry_userName = input("Member Name: \n")
+        entry_department = input("Who would you like to see, Manager or Teller? \n")
         entry_pinNum = input("Enter Pin Number: \n")
-        if (entry_birthPlace == self.birthPlace and entry_pinNum == self.pinNum):
-            print("Welcome back, {} Let's do some Banking!, for a nominal fee\n".format(entry_name))
+        if (entry_department == "Teller"): 
+            print("Welcome back, {}! Jimmy will be with you shortly \n".format(entry_userName))
+        elif (entry_department == "Manager"):
+            print("Welcome back, {}! Billy will be with you shortly \n".format(entry_userName))
         else:
-            print("Incorrect Pin Number. Card will be retained and security and local law enforcement are enroute!!\n")
-
-class ArmorCar:
-    driver = "Ted"
-    mover = "James"
-    truckNum = "0459"
+            print ("User Name, Pin Number, or desired employee incorrect. Goodbye")
 
 
-    def loginInfo(self):
-        entry_driver = input("Enter Drivers Name: \n")
-        entry_mover = input("Enter Name of Money Mover: \n")
-        entry_truckNum = input("Enter Truck Number: \n")
-        if (entry_truckNum == self.truckNum and entry_mover == self.mover):
-            print("Welcome back, {}! Thanks for keeping our Money safe\n".format(entry_mover))
-        else:
-            print("You are not, {}. Access to vault denied. Security and local law enforcement are enroute and YOU WILL BE SHOT!!\n".format(entry_mover))
 
-#  This code called the methods written above
-worker = Banker()
-worker.loginInfo()
+parents = Banker()
+parents.loginInfo()
 
-bmember = Member()
-bmember.loginInfo()
+kiddo = Underling()
+kiddo.loginInfo()
 
-armorm = ArmorCar()
-armorm.loginInfo()
+kiddo1 = Member()
+kiddo1.loginInfo()
